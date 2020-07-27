b92149a00e00cbd7852eb09488d16a52c59c8b2f
IsoerNC <67999634+IsoerNC@users.noreply.github.com>
2020-07-27 22:24:57 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,7 @@ function Vezrtical_move () {
     led.plot(X, Y)
 }
 function Horizontal_move () {
+    liste = [X, Y]
     while (X - Y < 0) {
         X += X - Y
         led.plot(X - Y, Y)
@@ -18,20 +19,22 @@ function Horizontal_move () {
         }
     }
 }
+let liste: number[] = []
 let Direction2 = 0
 let Y = 0
 let X = 0
+let Led_mouvante1 = 4
+let Led_mouvante_2 = 0
 X = 4
-Y = 0
-led.plot(X, Y)
+led.plot(Led_mouvante1, Led_mouvante_2)
 led.plot(X - 1, Y + 1)
 led.plot(X - 2, Y + 2)
 led.plot(X - 3, Y + 3)
 led.plot(X - 4, Y + 4)
 Direction2 = 1
 basic.forever(function () {
-    basic.pause(500)
+    basic.pause(100)
     Vezrtical_move()
     Horizontal_move()
-    basic.pause(500)
+    basic.pause(100)
 })
